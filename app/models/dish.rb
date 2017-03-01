3c1f45d3abca7eb165d5560dcf7289900b62ff5c
Raghu Betina <raghu@firstdraft.com>
2017-03-01 20:14:12 +0000

    Direct association between dishes and photos

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,9 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   has_many   :reviews,
              :dependent => :destroy
 
